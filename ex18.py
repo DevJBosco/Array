599e6f6bffb87c49157a228b8b80c2415e1b19d6
João Bosco <131721349+Novelvlr@users.noreply.github.com>
2023-10-10 11:05:46 -0300

    ex18.py

--- ex18.py
+++ ex18.py
@@ -1,8 +1,8 @@
+a = [2,5,4,2,8,5,2]
+b = [0,0,0,0,0,0,0]
 
-a=[2,5,4,2,8,5,2]
+for x in range (7):
+    b[6-x] = a[x]
+print(b)
 
-b= []
 
-for x in range(6,-1,-1):
-     b.append(a[x])
-print(f"b={b}")
