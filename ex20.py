2023caf5ab8c14b6f13b686e14b52e03902d79a6
João Bosco <131721349+Novelvlr@users.noreply.github.com>
2023-10-10 11:35:28 -0300

    ex20.py

--- ex20.py
+++ ex20.py
@@ -1,29 +1,17 @@
-numeros= []
+D = [0,0,0,0,0,0,0,0,0,0]
 
-for x in range (10):
-     num = float(input("Digite um número:"))
-     numeros.append(num)
+for y in range (10):
+     D[y]=int(input("digite um número: "))
 
-#listas separadas
+for z in range(10):
+    if D[z]%2!=0:
+        print(f"Este é impar: {D[z]}")
+    else:
+        print(f"Este é par: {D[z]}")
 
-numeros_pares = []
-numeros_impares = []
-numeros_zeros = []
-numeros_positivos = []
-numeros_negativos = []
-
-for num in (numeros):
-     if num %2==0:
-          numeros_impares.append(num)
-     else:
-          numeros_pares.append(num)
-     if num > 0:
-          numeros_positivos.append(num)
-     elif num < 0:
-          numeros_negativos.append(num)
-     elif num == 0:
-          numeros_zeros.append(num)
-
-print(f"Na lista  {numeros_pares} são números pares e {numeros_impares} números impáres")
-print(f"{numeros_positivos} são positivos e {numeros_negativos} são negativos.")
-print(f"{numeros_zeros} são zero")
+    if D[z]>0:
+        print(f"Este é positivo: {D[z]}")
+    elif D[z]<0:
+        priont(f"Este é negativo: {D[z]}")
+    else:
+        print(f"Zero {D[z]}")
